b08e9367191aea2a84d91683d037b8e4350ef6c9
DylanChambo <dylan.chambo@gmail.com>
2023-05-20 18:09:07 +1200

    feat: :sparkles: add basic click functionality

--- app/src/main/java/com/djliquor/app/MainActivity.java
+++ app/src/main/java/com/djliquor/app/MainActivity.java
@@ -2,6 +2,10 @@ package com.djliquor.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView backIcon = findViewById(R.id.back_icon);
+        ImageView searchIcon = findViewById(R.id.search_icon);
+        TextView toolbarTitle = findViewById(R.id.toolbar_title);
+        backIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked Back!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked Search!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        toolbarTitle.setText("DJ Liquor");
+
     }
 }

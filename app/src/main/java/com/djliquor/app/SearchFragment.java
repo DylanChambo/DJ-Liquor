febcfed2b9da1fa452e23cd10636da5a0c141ac7
DylanChambo <dylan.chambo@gmail.com>
2023-05-28 19:36:34 +1200

    feat: :sparkles: add other fragments

---
+++ app/src/main/java/com/djliquor/app/SearchFragment.java
@@ -0,0 +1,45 @@
+package com.djliquor.app;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.djliquor.app.databinding.FragmentSearchBinding;
+
+public class SearchFragment extends Fragment {
+
+    private FragmentSearchBinding binding;
+
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(HomeFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+//            }
+//        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}

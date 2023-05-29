2eb7768552f3eea5432275e62847bd36ec168f44
jker854 <jker854@aucklanduni.ac.nz>
2023-05-30 11:34:20 +1200

    Begun on provider class
    Added provider class, currently only has the names.

---
+++ Java/Project302/src/ProductProvider.java
@@ -0,0 +1,22 @@
+public class ProductProvider {
+
+    static String[] names = {"Haagen Lager", "Lion Red", "Heineken", "Heineken 0.0%", "Corona Extra",
+    "Speights", "Speights Summit Ultra Low Carb", "Export 33", "Peroni Nastro Azzurro", "Export Gold",
+            "Somersby Apple Cider", "Old Mout Feijoa & Cider", "Old Mout Cider Boysencider", "Zeffer Apple Crumble Cider", "Somersby Passionfruit & Orange Cider",
+            "Old Mout Cider Pear Scrumpy", "Old Mout Scrumpy Berry Cider", "Zeffer Rose with Berry Infusion Cider", "Somersby Pear Cider", "Old Mount Scrumpy Tropical Cider",
+            "Clean Collective Wild Berry & Lime Vodka", "Cruiser Cool Lime", "Long White Vodka Apple Pear", "Gordon's Gin & Tonic Slimline"
+            "Clean Collective Pink Gin", "Long White Vodka Passionfruit", "Rinse Lemon Vodka", "Cruiser Blueberry",
+            "Pals Vodka Peach Passionfruit & Soda", "Odd Company Vodka Raspberry & Pomegranate"};
+
+    static String[] wines = {"Stone Paddock Hawke's Bay Syrah 2021", "Little Giant Barossa Shiraz 2021", "Domaine de la Vinconniere, Muscadet Sur Lie 2021",
+            "Nero Oro Nero D'Avola Appassimento Sicilia DOC 2021", "Milenrama Rioja Reserva 2016", "Three Finger Jack Old Vine Zinfandel 2019",
+            "Momo Organic Marlborough Sauvignon Blanc 2022", "Te Awanga Estate Hawke's Bay Chardonnay 2022", "Momo Marlborough Pinot Noir 2022",
+            "Vavasour Awatere Valley Pinot Gris 2022"};
+
+    static String[] spirits = {"Altos Reposado Tequila", "Bumbu XO Rum", "Proper Twelve Irish Whiskey",
+            "Chivas Regal Extra 13YO Tequila Cask Blended Scotch Whisky", "Absolut Vodka Watermelon", "Langs Banana Rum",
+            "Bacardi Carta Blanca Superior White Rum", "Bacardi Spiced Rum", "Coruba Gold Rum", "Canadian Club Spiced Whisky",
+            "Jumping Goat Cold Brewed Coffee Whisky Liqueur", "Jumping Goat Cold Brewed Coffee Vodka Liqueur", "Aperol Aperitivo",
+            "Pimms No 1 Cup", "Bailey's Irish Cream", "Jagermeister Cold Brew Coffee", "Jagermeister", "Kahlua", "Malibu", "Kahlua Salted Caramel"};
+
+}

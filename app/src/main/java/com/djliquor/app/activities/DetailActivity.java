5a5168e095beb0ff049018a4478a12a00405e404
DylanChambo <dylan.chambo@gmail.com>
2023-05-31 15:38:39 +1200

    feat: :sparkles: add basic detail activity functionality

--- app/src/main/java/com/djliquor/app/activities/DetailActivity.java
+++ app/src/main/java/com/djliquor/app/activities/DetailActivity.java
@@ -1,9 +1,14 @@
 package com.djliquor.app.activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.djliquor.app.R;
 import com.djliquor.app.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
@@ -15,5 +20,17 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        SearchView searchView = (SearchView) this.findViewById(R.id.search_view);
+        ImageView backButton = (ImageView) this.findViewById(R.id.back_button);
+
+        searchView.setVisibility(View.INVISIBLE);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
